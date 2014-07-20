bd71bcd69ce755e52071d1d05cfb493e8c853580
Daniel Babicky <dudebrodudelol@gmail.com>
2014-07-20 15:16:39 -0700

    prepare for profiles

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+   validates :fname, length: { minimum: 2 }
+   validates :lname, length: { minimum: 2 }
+end
